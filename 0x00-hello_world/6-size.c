ea25718b609b58d4a76d03b498ee1f2f3fcd0d09
Your Name <you@example.com>
2023-02-16 14:55:06 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
  * main - Entry
  * Return: 0 (Sucess)
  */
-int (void)
+int main(void)
 {
 printf("Size of char: %i byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
@@ -11,3 +11,4 @@ printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
+}
