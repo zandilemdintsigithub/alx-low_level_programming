8beb6a22228c2d41fd2187472be1a7fd9eb48255
Your Name <you@example.com>
2023-02-27 09:37:16 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the value of two integers.
+ * @a: integer to  swap
+ * @b: integer to be swap
+ */
+void swap_int(int *a, int *b)
+{
+
+	int  m;
+
+	*a = *b;
+	*b = m;
+}
