eb4c2ca1ce5fd6116aef4b1d95c2b3c2e014141d
Your Name <you@example.com>
2023-02-27 10:38:54 -0800

    3-puts

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
+
 /**
- * swap_int - Swaps the value of two integers.
+ * swap_int - Swaps the value of two integers
  * @a: integer to  swap
  * @b: integer to be swap
  */
-
 void swap_int(int *a, int *b)
 {
 
 	int  m;
 	
+
 	m = *a;
 	*a = *b;
 	*b = m;
