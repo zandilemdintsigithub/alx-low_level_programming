b6f76a2ac529cdfd29a2e58a307e06f093aafb18
Your Name <you@example.com>
2023-03-01 12:20:23 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - copy string number
+ * @dest: store string of n byte into dest
+ * @src: input value of source string
+ * @n: value of bytes copied from src
+ * Return: string dest
+ */
+char *_strcpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	return (dest);
+}
