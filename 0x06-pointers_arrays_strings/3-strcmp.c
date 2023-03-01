41d8dcfeaac4cb79ccbb9466ac018912818ff0e1
Your Name <you@example.com>
2023-03-01 13:35:16 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - compare the two strings
+ * @s1: first string value
+ * @s2: second string value
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++
+	}
+	return (0);
+}
+
