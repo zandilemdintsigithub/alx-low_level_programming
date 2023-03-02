d800b9aff08ac16478344aabbd03a8d9f55d55ac
Your Name <you@example.com>
2023-03-02 01:04:21 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,36 @@
 #include "main.h"
 
+
 /**
  * cap_string - capilalize all words of string
  * @str: pointer to capitalize
  * Return: pointer to change string
  */
 char *cap_string(char *str)
+
 {
 	int index = 0;
 
 	while (str[index])
 	{
-
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-		         index++;
-
-		if (str[index - 1] == ' ' ||
-	   	    str[index - 1] == '\t' ||
-	            str[index - 1] == '\n' ||
-	            str[index - 1] == ',' ||
-	            str[index - 1] == ';' ||
-	            str[index - 1] == '.' ||
-	            str[index - 1] == '!' ||
-	            str[index - 1] == '?' ||
-	            str[index - 1] == '"' ||
-	            str[index - 1] == '(' ||
-	            str[index - 1] == ')' ||
-	            str[index - 1] == '{' ||
-	            str[index - 1] == '}' ||
-	            index == 0)
-		        str[index] -= 32;
-	       index++;
-	}
-
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
+        index++;
+	if (str[index - 1] == ' ' ||
+	str[index - 1] == '\t' ||
+	str[index - 1] == '\n' ||
+	str[index - 1] == ',' ||
+	str[index - 1] == ';' ||
+	str[index - 1] == '.' ||
+	str[index - 1] == '!' ||
+	str[index - 1] == '?' ||
+	str[index - 1] == '"' ||
+	str[index - 1] == '(' ||
+	str[index - 1] == ')' ||
+	str[index - 1] == '{' ||
+	str[index - 1] == '}' ||
+	index == 0)
+	str[index] -= 32;
+	index++;
+        } 
 	return (str);
 }
-	
-
