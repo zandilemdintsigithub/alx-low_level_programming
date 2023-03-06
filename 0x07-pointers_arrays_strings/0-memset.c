ce4c12cc950e72e15c6fc4c58e3d1ca5f9782712
Your Name <you@example.com>
2023-03-06 06:15:48 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,20 @@
 #include "main.h"
-
 /**
  * _memset - fill memory with constant byte
  * @s: memory to be filled
  * @b: constant byte
  * @n: number of bytes
+ *
  * Return: Always 0
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for  (i = 0; i < n; i++)
+	for  (; n > 0; i++)
+	{
 		s[i] = b;
+		n--;
+	}
 	return (s);
 }
