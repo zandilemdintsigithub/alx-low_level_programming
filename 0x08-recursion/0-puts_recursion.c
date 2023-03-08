65ce6e85225321adc4e3b6b8c5a5c8d38479fbf6
Your Name <you@example.com>
2023-03-08 04:52:51 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string, followed by a new line
+ * @s: string
+ * Return: no
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_putchar_recursion(s);
+}
