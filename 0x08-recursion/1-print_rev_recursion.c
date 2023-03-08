16dec48e564bb701fb691b13d2327c47e9c1ff87
Your Name <you@example.com>
2023-03-08 05:17:51 -0800

    -print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string in reverse
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_putchar_rev_recursion(s);
+	s--;
+	_putchar (*s);
+}
