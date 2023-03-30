db58b8f0ec95c93aa76cfb2746e84677fdf16990
zandile mdintsi <zandilemdintsi@gmail.com>
2023-03-30 13:18:50 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LINKED
+#define LINKED
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,9 +11,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -19,4 +22,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
+#endif /*LINKED*/
 
