9fc155d7b8bd9d7fed9baacdf5a6310881a6f9da
zandile mdintsi <zandilemdintsi@gmail.com>
2023-04-10 13:44:37 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,18 +24,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
-	if (fd == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	if (len > 0)
-	{
-		w = write(fd, text_content, len);
+	close(fd);
 
-		if (w == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	return (1);
 }
 
